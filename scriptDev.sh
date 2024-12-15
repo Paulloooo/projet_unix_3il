f89f8146d0ddc3ce9e4cbf11ba372138e4128ff2
Paulloooo <paul.biarneix@gmail.com>
2024-12-15 12:07:11 +0100

    On installe tout

--- scriptDev.sh
+++ scriptDev.sh
@@ -1,16 +1,52 @@
 #!/bin/bash
 
-echo -e "Avec quel serveur voulez-vous développer ?\n1 - Node JS\n2 - Django\n"
-read server
+SERVER=0
+VSCODE=0
 
+#On demande à l'utilisateur s'il veut développer avec Node JS ou Django
+while [ $SERVER -ne 1 ] && [ $SERVER -ne 2 ]; do
+	echo -e "Avec quel serveur voulez-vous développer ?\n1 - Node JS\n2 - Django\n"
+	read SERVER
+done
+
+#Suivant le choix, on vérifie si les paquets nécessaires sont installés
 if [ $SERVER -eq 1 ]; then
-	echo "nodeJS!!"
-elif [ $SERVER -eq 2 ]; then
-	echo "autre truc python nul !!"
-else 
-	echo "Vous avez entré une mauvaise option, recommencez !"
+	#On vérifie si Node JS est installé
+	if node -v &> /dev/null; then
+		echo "Node JS est installé"
+	else 
+		echo "Installation de Node JS..."
+		echo y | apt-get install nodejs > /dev/null 
+	fi
+
+	#On vérifie si NPM est installé
+	if npm -v &> /dev/null; then
+		echo "NPM est installé"
+	else 
+		echo "Installation de NPM..."
+		echo y | apt-get install npm > /dev/null 
+	fi
+fi 
+
+if [ $SERVER -eq 2 ]; then
+	#On vérifie si Python est installé
+	if python -V &> /dev/null; then
+		echo "Python est installé"
+	else 
+		echo "Installation de Python..."
+		echo y | apt-get install python > /dev/null 
+	fi
+
+	#On vérifie si Django est installé
+	if django-admin --version &> /dev/null; then
+		echo "Django est installé"
+	else 
+		echo "Installation de Django..."
+		echo y | apt-get install django > /dev/null 
+	fi
 fi
 
+#On vérifie si Git est installé
 if git -v &> /dev/null; then
 	echo "Git est installé"
 else 
@@ -18,3 +54,15 @@ else
 	echo y | apt-get install git > /dev/null 
 fi
 
+#Optionnel, mais on vérifie si l'utilisateur a VSCode
+#Si non, on lui propose de l'installer
+if code -v &> /dev/null; then
+	echo "Vous avez installé VS Code, bon choix :)"
+else 
+	echo -e "Voulez-vous installer Visual Studio Code ?\n1 - Oui\n2 - Non\n"
+	read VSCODE
+	if [ $VSCODE -eq 1 ]; then
+		echo "Installation de Visual Studio Code..."
+		echo y | apt-get install code > /dev/null 
+	fi
+fi
